e22e72e0cc5ede4f0bf90a4929f5d84a772b327b
AxelCH <axel.chuine.pro@gmail.com>
2025-09-26 11:38:06 +0200

    premier commit pour le projet medi-histo

---
+++ src/main/java/com/openclassrooms/medihisto/MediHistoApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.medihisto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediHistoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediHistoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openclassrooms/medihisto/MediHistoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.medihisto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediHistoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
